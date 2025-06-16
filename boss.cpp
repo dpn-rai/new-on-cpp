c5789071529928e87f1ea805ee4f7bc465853a4d
dpn-rai <drai68820@gmail.com>
2025-06-16 21:19:15 +0530

    add me

---
+++ boss.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+main(){
+    int a;
+    int b;
+    int c;
+    cin>>a>>b>>c;
+    if (a>b&&a>c) {
+        cout<< "a is greatest";}
+        else if (b>a&&b>c){
+        cout <<" b is greatest";
+        }
+    else{
+        cout <<"c is greatest";
+     }
+}
+    
